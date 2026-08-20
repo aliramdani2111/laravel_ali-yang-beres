aa188e653d0098278ca3d3fc6bdf8b3c85753d9d
ahmaddwi19 <duwibayu82@gmail.com>
2026-08-20 08:33:31 +0700

    commit

--- resources/views/siswas/edit.blade.php
+++ resources/views/siswas/edit.blade.php
@@ -2,27 +2,34 @@
 <html>
 <head>
     <title>Edit Siswa</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
+
+<div class="container">
 
     <h2>Edit Siswa</h2>
 
     <form action="{{ route('siswas.update', $siswa->id) }}" method="POST">
-    @csrf
-    @method('PUT')
+        @csrf
+        @method('PUT')
 
-<label>Nama:</label><br>
-<input type="text" name="nama" value="{{ $siswa->nama }}"><br><br>
+        <label>Nama:</label><br>
+        <input type="text" name="nama" value="{{ $siswa->nama }}"><br><br>
 
-<label>Kelas:</label><br>
-<input type="text" name="kelas" value="{{ $siswa->kelas }}"><br><br>
+        <label>Kelas:</label><br>
+        <input type="text" name="kelas" value="{{ $siswa->kelas }}"><br><br>
 
-        <button type="submit">Update</button>
+        <button type="submit" class="btn-edit">Update</button>
+
     </form>
 
     <br>
 
     <a href="{{ route('siswas.index') }}">Kembali</a>
+
+</div>
 
 </body>
 </html>
--- resources/views/siswas/index.blade.php
+++ resources/views/siswas/index.blade.php
@@ -2,29 +2,48 @@
 <html>
 <head>
     <title>Data Siswa</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 
+<div class="container">
+
     <h2>Data Siswa</h2>
 
-    <a href="{{ route('siswas.create') }}">+ Tambah Siswa</a>
-
-    <br><br>
+    <a href="{{ route('siswas.create') }}" class="btn-tambah">
+        + Tambah Siswa
+    </a>
 
     @foreach ($siswas as $siswa)
-        <p>
+
+        <div class="data-siswa">
+
             Nama: {{ $siswa->nama }} |
             Kelas: {{ $siswa->kelas }}
 
-            <a href="{{ route('siswas.edit', $siswa->id) }}">Edit</a>
+            <a href="{{ route('siswas.edit', $siswa->id) }}" class="btn-edit">
+                Edit
+            </a>
 
-            <form action="{{ route('siswas.destroy', $siswa->id) }}" method="POST" style="display:inline;">
+            <form action="{{ route('siswas.destroy', $siswa->id) }}"
+                  method="POST"
+                  style="display:inline;">
+
                 @csrf
                 @method('DELETE')
-                <button type="submit">Hapus</button>
+
+                <button type="submit" class="btn-hapus">
+                    Hapus
+                </button>
+
             </form>
-        </p>
+
+        </div>
+
     @endforeach
+
+</div>
 
 </body>
 </html>
